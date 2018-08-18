4c8e862cbd6af360cdcd3088824cdead2a01c64d
swithen-colaco <swithen_colaco@hotmail.com>
2018-08-18 20:33:13 +0530

    setup app for heroku

--- server1/db/mongoose.js
+++ server1/db/mongoose.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose').set('debug', true);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongodb://user:pass@123@ds125862.mlab.com:25862/node-todo-api || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }
+mongoose.connect('mongodb://user:pass123@ds125862.mlab.com:25862/node-todo-api' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }
 );
 
 module.exports = {
