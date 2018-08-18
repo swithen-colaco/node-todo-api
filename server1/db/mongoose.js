7e67a0891b43fdf1ccbcff5a712c5bb0ae52f376
swithen-colaco <swithen_colaco@hotmail.com>
2018-08-18 20:15:43 +0530

    setup app for heroku

--- server1/db/mongoose.js
+++ server1/db/mongoose.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose').set('debug', true);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }
 );
 
 module.exports = {
